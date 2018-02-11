ca7f60322b8bf0ffcf53523ae1c700ab4ff6b82f
BOX-O-T\Tristan <tristanmoore500@gmail.com>
2018-02-11 13:13:43 -0800

    added puzzle class

--- 15PuzzleHeuristicSolver/15PuzzleHeuristicSolver/Program.cs
+++ 15PuzzleHeuristicSolver/15PuzzleHeuristicSolver/Program.cs
@@ -1,15 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _15PuzzleHeuristicSolver
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            int[] spiralArr = new int[Puzzle.BOARD_SIZE] { 1, 2, Puzzle.BLANKVAL, 3, 6, 7, 11, 4, 5, 9, 12, 8, 13, 10, 14, 15 };
+            var spiralPuzz = new Puzzle(spiralArr);
+            //puzz.
+        }
+    }
+
+    internal class Puzzle
+    {
+        public const int BOARD_SIZE = 16;
+        public const int BOARD_WIDTH = 4;
+        public const int BLANKVAL = -1;
+        private static readonly int[] GOAL_STATE = new int[BOARD_SIZE] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, BLANKVAL };
+        private int[] tileSet;
+
+        public Puzzle(int[] board)
+        {
+            if (!IsValid(board))
+            {
+                tileSet = GOAL_STATE;
+                throw new Exception("ERROR, invalid board passed.");
+            }
+            tileSet = board;
+        }
+
+        public int BlankPos
+        {
+            get
+            {
+                return FindBlankIndex(tileSet);
+            }
+        }
+
+        public int InversionCount
+        {
+            get
+            {
+                return FindInversionCountInArray(tileSet);
+            }
+        }
+
+        private static bool IsValid(int[] boardArr)
+        {
+            if (boardArr.Length != BOARD_SIZE)
+            {
+                return false;
+            }
+            if (boardArr.Union(GOAL_STATE).Count() != boardArr.Intersect(GOAL_STATE).Count())
+            {
+                return false;
+            }
+
+            //If the blank is on an even row counting from the bottom(second-last, fourth - last etc), then the number of inversions in a solvable situation is odd.
+            //If the blank is on an odd row counting from the bottom(last, third-last, fifth - last etc) then the number of inversions in a solvable situation is even.
+            bool blankIsOnEvenRow = (FindBlankIndex(boardArr) / BOARD_WIDTH) % 2 == 0; //1 because its counting from the bottom
+            bool inversionCountIsEven = FindInversionCountInArray(boardArr) % 2 == 0;
+            return (blankIsOnEvenRow && !inversionCountIsEven) || (!blankIsOnEvenRow && inversionCountIsEven);
+        }
+
+        private static int FindBlankIndex(int[] boardArr)
+        {
+            return boardArr.Select((v, i) => new { Index = i, Value = v }).First(p => p.Value == BLANKVAL).Index;
+        }
+
+        private static int FindInversionCountInArray(int[] boardArr)
+        {
+            int invSum = 0;
+            for (int i = 0; i < BOARD_SIZE - 1; i++)
+            {
+                for (int j = i + 1; j < BOARD_SIZE; j++)
+                {
+                    if (boardArr[i] != BLANKVAL && boardArr[j] != BLANKVAL)
+                    {
+                        if (boardArr[i] > boardArr[j])
+                        {
+                            invSum++;
+                        }
+                    }
+                }
+            }
+            return invSum;
         }
     }
 }
