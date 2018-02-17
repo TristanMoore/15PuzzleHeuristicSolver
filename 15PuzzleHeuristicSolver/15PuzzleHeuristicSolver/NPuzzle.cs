ed6438afe58aa0e1507e2d47c59c0a9fe9a642ac
BOX-O-T\Tristan <tristanmoore500@gmail.com>
2018-02-17 02:44:14 -0800

    wrote n-puzzle solver and confirmed it works on small dataset

---
+++ 15PuzzleHeuristicSolver/15PuzzleHeuristicSolver/NPuzzle.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15PuzzleHeuristicSolver
+{
+    public class NPuzzle
+    {
+        public readonly Node initialState;
+        public readonly Node goalState; //starting at 1
+        public const int BLANKVAL = -1;
+
+        public enum Operator { Root, Up, Down, Left, Right };
+
+        public NPuzzle(Node init)
+        {
+            initialState = init;
+            int[] tmp = new int[init.state.Length];
+            goalState = new Node(tmp, init.rows, init.columns, 0, Operator.Root);
+            for (int i = 0; i < goalState.state.Length; i++)
+            {
+                goalState.state[i] = i;
+            }
+            goalState.state[goalState.state.Length - 1] = BLANKVAL;
+        }
+
+        public List<Node> SolveWithBestFirst()
+        {
+            List<Node> open = new List<Node>();
+            List<Node> close = new List<Node>();
+            Node best;
+            //1. put IS on open
+            open.Add(initialState);
+
+            while (true)
+            {
+                //2. if open is empty, fail
+                if (!open.Any())
+                {
+                    return null;
+                }
+
+                //pick out best from open and put on close
+                best = open.First();
+                foreach (Node n in open)
+                {
+                    if (n.HVal < best.HVal)
+                    {
+                        best = n;
+                    }
+                }
+                open.Remove(best);
+                close.Add(best);
+
+                //3. expand n
+                foreach (Operator op in GetValidOperators(best))
+                {
+                    Node successor = ApplyOperator(op, best);
+                    if (successor.Equals(goalState))
+                    {
+                        //4. if successor is goal, output solution
+                        return GenerateSolutionPath(close, successor);
+                    }
+
+                    Node duplicateClose = close.FirstOrDefault(n => successor.Equals(n));
+                    if (duplicateClose == null)
+                    {
+                        Node duplicateOpen = open.FirstOrDefault(n => successor.Equals(n));
+                        if (duplicateOpen == null)
+                        {
+                            open.Add(successor);
+                        }
+                        else if (duplicateOpen.HVal > successor.HVal)
+                        {
+                            open.Remove(duplicateOpen);
+                            open.Add(successor);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void PrintNode(Node n)
+        {
+            StringBuilder stb = new StringBuilder();
+
+            stb.AppendLine(new string('-', 20));
+            stb.AppendLine(string.Format("Operator: {0}", n.appliedOp.ToString()));
+            for (int i = 0; i < n.state.Length; i++)
+            {
+                if (i % n.columns == 0 && i != 0)
+                {
+                    stb.AppendLine();
+                }
+                stb.AppendFormat("{0} ", (n.state[i] + 1).ToString().PadLeft(2));
+            }
+            stb.AppendLine();
+            stb.AppendLine(new string('-', 20));
+
+            Console.WriteLine(stb);
+        }
+
+        private static List<Node> GenerateSolutionPath(List<Node> closed, Node finalState)
+        {
+            List<Node> solution = new List<Node>();
+
+            //backtrack through closed to get a path
+            solution.Add(finalState);
+            Node tmp = finalState;
+            while (tmp.appliedOp != Operator.Root)
+            {
+                tmp = ApplyReverseOperator(tmp.appliedOp, tmp);
+                solution.Add(tmp);
+                tmp = closed.First(n => n.Equals(tmp));
+            }
+
+            //reverse the backwards path
+            solution.First().appliedOp = Operator.Root;
+            foreach (Node n in solution)
+            {
+                n.appliedOp = ReverseOperator(n.appliedOp);
+            }
+            solution.Reverse();
+            return solution;
+        }
+
+        private static Operator ReverseOperator(Operator op)
+        {
+            switch (op)
+            {
+                case Operator.Up:
+                    return Operator.Down;
+
+                case Operator.Down:
+                    return Operator.Up;
+
+                case Operator.Left:
+                    return Operator.Right;
+
+                case Operator.Right:
+                    return Operator.Left;
+
+                case Operator.Root:
+                default:
+                    return op;
+            }
+        }
+
+        private static Node ApplyReverseOperator(Operator op, Node n)
+        {
+            return ApplyOperator(ReverseOperator(op), n);
+        }
+
+        private static Node ApplyOperator(Operator op, Node n) //assumes operation is valid
+        {
+            int blankIndex = FindBlankIndex(n);
+            int[] tmp = new int[n.state.Length];
+            n.state.CopyTo(tmp, 0);
+            switch (op)
+            {
+                case Operator.Up:
+                    tmp[blankIndex] = tmp[blankIndex - n.columns];
+                    tmp[blankIndex - n.columns] = BLANKVAL;
+                    break;
+
+                case Operator.Down:
+                    tmp[blankIndex] = tmp[blankIndex + n.columns];
+                    tmp[blankIndex + n.columns] = BLANKVAL;
+                    break;
+
+                case Operator.Left:
+                    tmp[blankIndex] = tmp[blankIndex - 1];
+                    tmp[blankIndex - 1] = BLANKVAL;
+                    break;
+
+                case Operator.Right:
+                    tmp[blankIndex] = tmp[blankIndex + 1];
+                    tmp[blankIndex + 1] = BLANKVAL;
+                    break;
+
+                case Operator.Root:
+                default:
+                    break;
+            }
+
+            return new Node(tmp, n.rows, n.columns, n.distance + 1, op);
+        }
+
+        private static List<Operator> GetValidOperators(Node n)
+        {
+            return Enum.GetValues(typeof(Operator)).Cast<Operator>().Where(op => OperatorIsValid(op, n)).ToList();
+        }
+
+        private static bool OperatorIsValid(Operator op, Node n)
+        {
+            int blankIndex = FindBlankIndex(n);
+
+            switch (op)
+            {
+                case Operator.Up:
+                    return blankIndex >= n.columns;
+
+                case Operator.Down:
+                    return blankIndex < n.state.Length - n.columns;
+
+                case Operator.Left:
+                    return blankIndex % n.columns != 0;
+
+                case Operator.Right:
+                    return blankIndex % n.columns != n.columns - 1;
+
+                default:
+                    return false;
+            }
+        }
+
+        private static int FindBlankIndex(Node n)
+        {
+            for (int i = 0; i < n.rows * n.columns; i++)
+            {
+                if (n.state[i] == BLANKVAL)
+                {
+                    return i;
+                }
+            }
+            throw new Exception("Blank not found.");
+        }
+
+        public static int FindHeuristic(Node n)
+        {
+            return FindManhattanDistance(n) + FindLinearConflict(n);
+        }
+
+        public static int FindManhattanDistance(Node n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n.rows * n.columns; i++)
+            {
+                if (n.state[i] != BLANKVAL)
+                {
+                    int currRow = i / n.rows;
+                    int currCol = i % n.columns;
+                    int destRow = n.state[i] / n.rows;
+                    int destCol = n.state[i] % n.columns;
+                    sum += Math.Abs(currRow - destRow);
+                    sum += Math.Abs(currCol - destCol);
+                }
+            }
+            return sum;
+        }
+
+        public static int FindLinearConflict(Node n)
+        {
+            int sum = 0;
+            for (int j = 0; j < n.rows; j++)
+            {
+                for (int k = 0; k < n.rows; k++)
+                {
+                    int jCurrRow = n.state[j] / n.columns;
+                    int kCurrRow = n.state[k] / n.columns;
+                    int jGoalRow = j / n.columns;
+                    int kGoalRow = k / n.columns;
+                    bool allOnSameRow = jCurrRow == kCurrRow && kCurrRow == jGoalRow && jGoalRow == kGoalRow;
+                    bool jIsRightOfK = n.state[k] < n.state[j];
+                    bool jGoalIsLeftOfGoalK = j < k;
+                    if (allOnSameRow && jIsRightOfK && jGoalIsLeftOfGoalK)
+                    {
+                        sum += 2;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+
+    public class Node
+    {
+        public NPuzzle.Operator appliedOp;
+        public int[] state;
+        public readonly int rows;
+        public readonly int columns;
+        public readonly int heuristic;
+        public readonly int distance;
+        public int HVal { get { return heuristic + distance; } }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="boardArry">Make this 0 based indexed. -1 is the blank.</param>
+        /// <param name="rows">1 indexed based</param>
+        /// <param name="colums">1 indexed based</param>
+        public Node(int[] boardArry, int rows, int colums, int currentDistance, NPuzzle.Operator previousOp)
+        {
+            this.rows = rows;
+            this.columns = colums;
+            state = boardArry;
+            heuristic = NPuzzle.FindHeuristic(this);
+            distance = currentDistance;
+            appliedOp = previousOp;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Node n = obj as Node;
+            if (n == null)
+            {
+                return false;
+            }
+            bool b = Enumerable.SequenceEqual(this.state, n.state);
+            return b;
+        }
+    }
+}
--- 15PuzzleHeuristicSolver/15PuzzleHeuristicSolver/Program.cs
+++ 15PuzzleHeuristicSolver/15PuzzleHeuristicSolver/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace _15PuzzleHeuristicSolver
 {
@@ -8,165 +9,296 @@ namespace _15PuzzleHeuristicSolver
     {
         private static void Main(string[] args)
         {
-            int[] spiralArr = new int[Puzzle.BOARD_SIZE] { 1, 2, Puzzle.BLANKVAL, 3, 6, 7, 11, 4, 5, 9, 12, 8, 13, 10, 14, 15 };
-            var spiralPuzz = new Puzzle(spiralArr);
-            //puzz.
+            //int[] test = { 0, 2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, -1 };
+            //Node init = new Node(test, 4, 4);
+
+            //int lc = NPuzzle.FindLinearConflict(init); //should be 2
+            //int md = NPuzzle.FindManhattanDistance(init); //should be 2
+
+            int[] board1 = { 0, 1, -1, 2, 5, 6, 10, 3, 4, 8, 11, 7, 12, 9, 13, 14 };
+            int[] board2 = { 0, 1, 2, 3, 11, 12, 13, 4, 10, -1, 14, 5, 9, 8, 7, 6 }; //spiral
+            int[] board3 = { -1, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }; //backward
+            Node init1 = new Node(board1, 4, 4, 0, NPuzzle.Operator.Root);
+            Node init2 = new Node(board2, 4, 4, 0, NPuzzle.Operator.Root);
+            Node init3 = new Node(board3, 4, 4, 0, NPuzzle.Operator.Root);
+            NPuzzle puzz1 = new NPuzzle(init1);
+            NPuzzle puzz2 = new NPuzzle(init2);
+            NPuzzle puzz3 = new NPuzzle(init3);
+
+            var n1 = NPuzzle.FindHeuristic(init1);
+            var n2 = NPuzzle.FindHeuristic(init2);
+            var n3 = NPuzzle.FindHeuristic(init3);
+
+            var solution1 = puzz1.SolveWithBestFirst();
+            foreach (Node n in solution1)
+            {
+                NPuzzle.PrintNode(n);
+            }
+
+            var solution2 = puzz2.SolveWithBestFirst();
+            foreach (Node n in solution2)
+            {
+                NPuzzle.PrintNode(n);
+            }
+
+            var solution3 = puzz3.SolveWithBestFirst();
+            foreach (Node n in solution3)
+            {
+                NPuzzle.PrintNode(n);
+            }
         }
     }
 
-    public class Puzzle
-    {
-        public const int BOARD_SIZE = 16;
-        public const int BOARD_WIDTH = 4;
-        public const int BLANKVAL = -1;
-        private const int CHANGED_FLAG = -42;
-        private static readonly int[] GOAL_STATE = new int[BOARD_SIZE] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, BLANKVAL };
-        private int[] tileSet;
+    //public class Puzzle
+    //{
+    //    public const int BOARD_SIZE = 16;
+    //    public const int BOARD_WIDTH = 4;
+    //    public const int BLANKVAL = -1;
+    //    private const int CHANGED_FLAG = -42;
+    //    private static readonly int[] GOAL_STATE = new int[BOARD_SIZE] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, BLANKVAL };
+    //    private int[] currTileSet;
 
-        public enum Operators { Up, Down, Left, Right };
+    //    public enum Operator { Up, Down, Left, Right };
 
-        public Puzzle(int[] board)
-        {
-            if (!BoardIsValidAndSolvable(board))
-            {
-                tileSet = GOAL_STATE;
-                throw new Exception("ERROR, invalid board passed.");
-            }
-            tileSet = board;
-        }
+    //    public Puzzle(int[] board)
+    //    {
+    //        if (!BoardIsValidAndSolvable(board))
+    //        {
+    //            currTileSet = GOAL_STATE;
+    //            throw new Exception("ERROR, invalid board passed.");
+    //        }
+    //        currTileSet = board;
+    //    }
 
-        public List<Operators> ValidOperators
-        {
-            get
-            {
-                return Enum.GetValues(typeof(Operators)).Cast<Operators>().Where(op => OperatorIsValid(op)).ToList();
-            }
-        }
+    //    public static List<Operator> FindValidOperators(Node n)
+    //    {
+    //        return Enum.GetValues(typeof(Operator)).Cast<Operator>().Where(op => OperatorIsValid(op, n)).ToList();
+    //    }
 
-        private bool OperatorIsValid(Operators op)
-        {
-            switch (op)
-            {
-                case Operators.Up:
-                    return BlankIndex > 3;
+    //    private static bool OperatorIsValid(Operator op, Node n)
+    //    {
+    //        int blankIndex = FindBlankIndex(n.state);
 
-                case Operators.Down:
-                    return BlankIndex < 12;
+    //        switch (op)
+    //        {
+    //            case Operator.Up:
+    //                return blankIndex > 3;
 
-                case Operators.Left:
-                    return BlankIndex % BOARD_WIDTH != 0;
+    //            case Operator.Down:
+    //                return blankIndex < 12;
 
-                case Operators.Right:
-                    return BlankIndex % BOARD_WIDTH != 3;
+    //            case Operator.Left:
+    //                return blankIndex % BOARD_WIDTH != 0;
 
-                default:
-                    return false;
-            }
-        }
+    //            case Operator.Right:
+    //                return blankIndex % BOARD_WIDTH != 3;
 
-        public void DoOperator(Operators op)
-        {
-            if (!OperatorIsValid(op))
-            {
-                throw new Exception("ERROR, Attempted to perform invalid operator.");
-            }
+    //            default:
+    //                return false;
+    //        }
+    //    }
 
-            int tmpIndex = BlankIndex;
-            switch (op)
-            {
-                case Operators.Up:
-                    tileSet[tmpIndex] = tileSet[tmpIndex - 4];
-                    tileSet[tmpIndex - 4] = BLANKVAL;
-                    break;
+    //    public Node DoOperator(Operator op, Node n)
+    //    {
+    //        Node nCopy = n.Copy();
+    //        if (!OperatorIsValid(op, nCopy))
+    //        {
+    //            throw new Exception("ERROR, Attempted to perform invalid operator.");
+    //        }
 
-                case Operators.Down:
-                    tileSet[tmpIndex] = tileSet[tmpIndex + 4];
-                    tileSet[tmpIndex + 4] = BLANKVAL;
-                    break;
+    //        int tmpIndex = FindBlankIndex(nCopy.state);
+    //        switch (op)
+    //        {
+    //            case Operator.Up:
+    //                nCopy.state[tmpIndex] = nCopy.state[tmpIndex - 4];
+    //                nCopy.state[tmpIndex - 4] = BLANKVAL;
+    //                break;
 
-                case Operators.Left:
-                    tileSet[tmpIndex] = tileSet[tmpIndex - 1];
-                    tileSet[tmpIndex - 1] = BLANKVAL;
-                    break;
+    //            case Operator.Down:
+    //                nCopy.state[tmpIndex] = nCopy.state[tmpIndex + 4];
+    //                nCopy.state[tmpIndex + 4] = BLANKVAL;
+    //                break;
 
-                case Operators.Right:
-                    tileSet[tmpIndex] = tileSet[tmpIndex + 1];
-                    tileSet[tmpIndex + 1] = BLANKVAL;
-                    break;
-            }
+    //            case Operator.Left:
+    //                nCopy.state[tmpIndex] = nCopy.state[tmpIndex - 1];
+    //                nCopy.state[tmpIndex - 1] = BLANKVAL;
+    //                break;
 
-            buffBlankIndex = CHANGED_FLAG;
-            buffInversionCount = CHANGED_FLAG;
-        }
+    //            case Operator.Right:
+    //                nCopy.state[tmpIndex] = nCopy.state[tmpIndex + 1];
+    //                nCopy.state[tmpIndex + 1] = BLANKVAL;
+    //                break;
+    //        }
 
-        private int buffBlankIndex = CHANGED_FLAG;
+    //        return nCopy;
+    //    }
 
-        public int BlankIndex
-        {
-            get
-            {
-                if (buffBlankIndex == CHANGED_FLAG)
-                {
-                    buffBlankIndex = FindBlankIndex(tileSet);
-                }
-                return buffBlankIndex;
-            }
-        }
+    //    public Node DoReverseOperator(Operator op, Node n)
+    //    {
+    //        switch (op)
+    //        {
+    //            case Operator.Up:
+    //                return DoOperator(Operator.Down, n);
 
-        private int buffInversionCount = CHANGED_FLAG;
+    //            case Operator.Down:
+    //                return DoOperator(Operator.Up, n);
 
-        public int InversionCount
-        {
-            get
-            {
-                if (buffInversionCount == CHANGED_FLAG)
-                {
-                    buffInversionCount = FindInversionCountInArray(tileSet);
-                }
-                return buffInversionCount;
-            }
-        }
+    //            case Operator.Left:
+    //                return DoOperator(Operator.Left, n);
 
-        private static bool BoardIsValidAndSolvable(int[] boardArr)
-        {
-            if (boardArr.Length != BOARD_SIZE)
-            {
-                return false;
-            }
-            if (boardArr.Union(GOAL_STATE).Count() != boardArr.Intersect(GOAL_STATE).Count())
-            {
-                return false;
-            }
+    //            case Operator.Right:
+    //                return DoOperator(Operator.Right, n);
+    //        }
 
-            //If the blank is on an even row counting from the bottom(second-last, fourth - last etc), then the number of inversions in a solvable situation is odd.
-            //If the blank is on an odd row counting from the bottom(last, third-last, fifth - last etc) then the number of inversions in a solvable situation is even.
-            bool blankIsOnEvenRow = (FindBlankIndex(boardArr) / BOARD_WIDTH) % 2 == 0; //1 because its counting from the bottom
-            bool inversionCountIsEven = FindInversionCountInArray(boardArr) % 2 == 0;
-            return (blankIsOnEvenRow && !inversionCountIsEven) || (!blankIsOnEvenRow && inversionCountIsEven);
-        }
+    //        return n;
+    //    }
 
-        private static int FindBlankIndex(int[] boardArr)
-        {
-            return boardArr.Select((v, i) => new { Index = i, Value = v }).First(p => p.Value == BLANKVAL).Index;
-        }
+    //    private static bool BoardIsValidAndSolvable(int[] boardArr)
+    //    {
+    //        if (boardArr.Length != BOARD_SIZE)
+    //        {
+    //            return false;
+    //        }
+    //        if (boardArr.Union(GOAL_STATE).Count() != boardArr.Intersect(GOAL_STATE).Count())
+    //        {
+    //            return false;
+    //        }
 
-        private static int FindInversionCountInArray(int[] boardArr)
-        {
-            int invSum = 0;
-            for (int i = 0; i < BOARD_SIZE - 1; i++)
-            {
-                for (int j = i + 1; j < BOARD_SIZE; j++)
-                {
-                    if (boardArr[i] != BLANKVAL && boardArr[j] != BLANKVAL)
-                    {
-                        if (boardArr[i] > boardArr[j])
-                        {
-                            invSum++;
-                        }
-                    }
-                }
-            }
-            return invSum;
-        }
-    }
+    //        //If the blank is on an even row counting from the bottom(second-last, fourth - last etc), then the number of inversions in a solvable situation is odd.
+    //        //If the blank is on an odd row counting from the bottom(last, third-last, fifth - last etc) then the number of inversions in a solvable situation is even.
+    //        bool blankIsOnEvenRow = (FindBlankIndex(boardArr) / BOARD_WIDTH) % 2 == 0; //1 because its counting from the bottom
+    //        bool inversionCountIsEven = FindInversionCountInArray(boardArr) % 2 == 0;
+    //        return (blankIsOnEvenRow && !inversionCountIsEven) || (!blankIsOnEvenRow && inversionCountIsEven);
+    //    }
+
+    //    private static int FindBlankIndex(int[] boardArr)
+    //    {
+    //        return boardArr.Select((v, i) => new { Index = i, Value = v }).First(p => p.Value == BLANKVAL).Index;
+    //    }
+
+    //    private static int FindInversionCountInArray(int[] boardArr)
+    //    {
+    //        int invSum = 0;
+    //        for (int i = 0; i < BOARD_SIZE - 1; i++)
+    //        {
+    //            for (int j = i + 1; j < BOARD_SIZE; j++)
+    //            {
+    //                if (boardArr[i] != BLANKVAL && boardArr[j] != BLANKVAL)
+    //                {
+    //                    if (boardArr[i] > boardArr[j])
+    //                    {
+    //                        invSum++;
+    //                    }
+    //                }
+    //            }
+    //        }
+    //        return invSum;
+    //    }
+
+    //    public List<Operator> SolveWithBestFirst()
+    //    {
+    //        List<Operator> solution = new List<Operator>();
+    //        List<Node> open = new List<Node>();
+    //        List<Node> close = new List<Node>();
+    //        Node goal = new Node(GOAL_STATE, 0);
+    //        Node initialState = new Node(currTileSet, FindInversionCountInArray(currTileSet));
+
+    //        //put IS on open
+    //        open.Add(initialState);
+
+    //        while (true)
+    //        {
+    //            //if open is empty, fail
+    //            if (!open.Any())
+    //            {
+    //                return null;
+    //            }
+
+    //            //take best from open and put on close
+    //            open = open.OrderBy(n => n.hVal).ToList();
+    //            var best = open[0];
+    //            open.RemoveAt(0);
+    //            close.Add(best);
+
+    //            //expand best and add successors to open
+    //            var validOps = FindValidOperators(best);
+    //            foreach (Operator op in validOps)
+    //            {
+    //                var n = DoOperator(op, best);
+    //                if (!close.Any(tmp => tmp.Equals(n)) && !open.Any(tmp => tmp.Equals(n)))
+    //                {
+    //                    n.backPtr = op;
+    //                    n.hVal = FindInversionCountInArray(n.state);
+    //                    open.Add(n);
+
+    //                    if (n == goal)
+    //                    {
+    //                        return TraverseSolution(initialState, n, close);
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+
+    //    public List<Operator> TraverseSolution(Node initialState, Node goalState, List<Node> closed)
+    //    {
+    //        List<Operator> solution = new List<Operator>();
+    //        Node curr = goalState;
+
+    //        while (curr != initialState)
+    //        {
+    //            solution.Add(curr.backPtr);
+    //            curr = DoReverseOperator(curr.backPtr, curr);
+    //            curr = closed.First(n => n.Equals(curr));
+    //        }
+
+    //        solution.Reverse();
+    //        return solution;
+    //    }
+    //}
+
+    //public class Node
+    //{
+    //    public int[] state;
+    //    public Puzzle.Operator backPtr;
+    //    public int hVal;
+
+    //    public Node(int[] boardArr, int inversionCt)
+    //    {
+    //        state = boardArr;
+    //        hVal = inversionCt;
+    //    }
+
+    //    public override bool Equals(object obj)
+    //    {
+    //        Node n = obj as Node;
+    //        if (n == null)
+    //        {
+    //            return false;
+    //        }
+    //        bool b = Enumerable.SequenceEqual(this.state, n.state);
+    //        return b;
+    //    }
+
+    //    public override string ToString()
+    //    {
+    //        StringBuilder stb = new StringBuilder();
+    //        foreach (int tile in state)
+    //        {
+    //            stb.Append(tile);
+    //            stb.Append(' ');
+    //        }
+    //        return stb.ToString();
+    //    }
+
+    //    public override int GetHashCode()
+    //    {
+    //        return this.ToString().GetHashCode();
+    //    }
+
+    //    public Node Copy()
+    //    {
+    //        return new Node((int[])state.Clone(), hVal);
+    //    }
+    //}
 }
